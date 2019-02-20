ced5bf217b2ac683681d57db1582c84f4af79f3b
Krzysztof Szustkiewicz <szustkiewicz.k@gmail.com>
2019-02-20 22:38:23 +0100

    database ORM

---
+++ Football/Model/Players.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Football.Model
+{
+    public partial class Players
+    {
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+    }
+}
---
+++ Football/Model/PlayersDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Football.Model
+{
+    public partial class PlayersDBContext : DbContext
+    {
+        public PlayersDBContext()
+        {
+        }
+
+        public PlayersDBContext(DbContextOptions<PlayersDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Players> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=PlayersDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.2-servicing-10034");
+
+            modelBuilder.Entity<Players>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.PlayerName)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
