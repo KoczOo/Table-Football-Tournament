17a0098cc4712d9e65650401a267019c5aaf6e2e
acichecki <a.cichecki@gmail.com>
2019-02-20 19:06:58 +0100

    TournamentTree codebehind

--- Football/Pages/TournamentTree.cshtml.cs
+++ Football/Pages/TournamentTree.cshtml.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Football.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Football.Pages
 {
     public class TournamentTreeModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _db;
 
+        public TournamentTreeModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public DbSet<Player> Players { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            return Page();
         }
     }
 }
